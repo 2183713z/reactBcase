9f17f2fc136066e65f208da5b7b14f912c89bd63
Your Name <you@example.com>
2016-10-13 19:11:39 +0800

    home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,6 +7,12 @@ class Home extends React.Component {
         fontSize:'42px',
         fontWeight:'700',
         marginBottom:'20px'
+      },
+      music:{
+        position:'absolute',
+        left:0,
+        top:0,
+        zIndex:'-100'
       }
     }
     return(
@@ -15,8 +21,11 @@ class Home extends React.Component {
             <div className='home-item'>
               <h2 style={styles.h2}>I am <span style={{color:'#00bcd4'}}>Susan</span></h2>
               <p style={{marginBottom:'20px'}}>WEB DEVELOPER</p>
-              <button className='home-btn'><a href='http://github.com/2183713z' style={{color:'#fff'}}>Cliclk Me</a></button>
+              <button className='home-btn'><a href='http://github.com/2183713z' style={{color:'#fff'}} className='click'>Cliclk Me</a></button>
             </div>
+            <audio style={styles.music} src="http://newming.github.io/100/9/%E5%BA%84%E5%BF%83%E5%A6%8D%20-%20%E8%B6%8A%E7%AC%91%E8%B6%8A%E9%9A%BE%E8%BF%87.mp3" controls autoPlay loop>
+        	 	<p>您的浏览器不支持Html audio</p>
+        	 </audio>
           </div>
         </div>
     )
--- src/Work/Work.js
+++ src/Work/Work.js
@@ -9,30 +9,36 @@ class Work extends React.Component {
               <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain-none.jpg"/>
               <div className='work-text'>
                 <h3>Jquery--demo</h3>
-                <p>2016-9-22</p>
+                <p>2016-5-16</p>
               </div>
             </a>
           </div>
         <div className='work-card'>
-          <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain4.jpg"/>
-          <div className='work-text'>
-            <h3>初来乍到</h3>
-            <p>2016-9-22</p>
-          </div>
+          <a href='http://2183713z.github.io/9-18-Demo'>
+            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain4.jpg"/>
+            <div className='work-text'>
+              <h3>React--demo</h3>
+              <p>2016-9-18</p>
+            </div>
+          </a>
         </div>
         <div className='work-card'>
-          <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain1.jpg"/>
-          <div className='work-text'>
-            <h3>初来乍到</h3>
-            <p>2016-9-22</p>
-          </div>
+          <a href='#'>
+            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain1.jpg"/>
+            <div className='work-text'>
+              <h3>express && React</h3>
+              <p>2016-9-22</p>
+            </div>
+          </a>
         </div>
         <div className='work-card'>
-          <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg"/>
-          <div className='work-text'>
-            <h3>初来乍到</h3>
-            <p>2016-9-22</p>
-          </div>
+          <a href='https://2183713z.github.io/small-case' >
+            <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg"/>
+            <div className='work-text'>
+              <h3>small-case</h3>
+              <p>2016-6-19</p>
+            </div>
+          </a>
         </div>
 
       </div>
